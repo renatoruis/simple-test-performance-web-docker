06e39464869e0813a4edcd74f0a884ee056f593a
Renato Ruis <renatoruis@gmail.com>
2019-03-07 16:47:16 -0300

    update

--- get.sh
+++ get.sh
@@ -4,6 +4,8 @@
 x=1
 while [ $x -le $TIMES ]
 do
-  echo "$x Response: $(curl -sIk --insecure -H 'Cache-Control: no-cache' https://$URL?$(date +%s) | head -n 1|cut -d' ' -f2)"
+  URL="https://$URL/?$(date +%s)"
+  echo "Attempt: $x | Response: $(curl -sIk --insecure -H 'Cache-Control: no-cache' $URL | head -n 1|cut -d' ' -f2) | $URL"
+  sleep 1
   x=$(( $x + 1 ))
 done
